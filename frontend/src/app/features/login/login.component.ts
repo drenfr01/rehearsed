f843772bb1ddeadd86c96d3dbde397eaf38f321e
Duncan Renfrow-Symon <duncanrenfrow@Duncans-MacBook-Pro-2.local>
2025-11-08 15:09:34 -0500

    adding loading spinner and other small attributes

--- frontend/src/app/features/login/login.component.ts
+++ frontend/src/app/features/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrl: './login.css',
 })
 export class Login {
+  private router = inject(Router);
+  private authService = inject(AuthService);
+  private isLoading = signal(false);
+  private destroyRef = inject(DestroyRef);
+  private error = signal<string>('');
 
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  onSubmit() {
+    this.isLoading.set(true);
+    const subscription = this.authService.login(this.form.value.email!, this.form.value.password!).subscribe({
+      next: () => {
+        this.router.navigate(['/app/classroom'], { replaceUrl: true });
+      },
+      error: (error: Error) => {
+        this.error.set(error.message);
+        this.isLoading.set(false);
+      },
+      complete: () => {
+        this.isLoading.set(false);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }

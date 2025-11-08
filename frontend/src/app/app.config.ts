28433553d658c98ad2a7693b7ae2993994c98570
Duncan Renfrow-Symon <duncanrenfrow@Duncans-MacBook-Pro-2.local>
2025-11-08 15:24:21 -0500

    plumbing is complete

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,11 +2,24 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpEvent, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+function bearerTokenInterceptor(request: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> {
+  const token = localStorage.getItem('token');
+  if (token) {
+    request = request.clone({
+      setHeaders: { Authorization: `Bearer ${token}` }
+    });
+  }
+  return next(request);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([bearerTokenInterceptor]))
   ]
 };
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './core/components/header/header.component';
+import { Footer } from './core/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

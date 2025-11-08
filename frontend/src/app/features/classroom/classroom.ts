8d7c2687a2fc8769a9c2148766139a9cf623edd7
Duncan Renfrow-Symon <duncanrenfrow@Duncans-MacBook-Pro-2.local>
2025-11-08 18:55:32 -0500

    adding skeleton of classroom component

--- frontend/src/app/features/classroom/classroom.ts
+++ frontend/src/app/features/classroom/classroom.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { ChatGraphService } from '../../core/services/chat-graph.service';
+import { ChatRequest } from '../../core/models/chat-graph.model';
 
 @Component({
   selector: 'app-classroom',
@@ -8,4 +10,36 @@ import { Component } from '@angular/core';
 })
 export class Classroom {
 
+  protected isLoading = signal(false);
+  protected error = signal<string>('');
+  private chatGraphService = inject(ChatGraphService);
+  private destroyRef = inject(DestroyRef);
+
+  protected userInput = signal<string>('');
+  protected isApproved = signal<boolean>(false);
+
+  onSubmit() {
+    this.isLoading.set(true);
+    this.error.set('');
+    const newChatRequest: ChatRequest = {
+      is_resumption: true,
+      resumption_text: this.userInput()!,
+      resumption_approved: this.isApproved()!,
+      messages: [],
+    }
+    const subscription = this.chatGraphService.sendGraphRequest(newChatRequest).subscribe({
+      error: (error: Error) => {
+        this.error.set(error.message);
+        this.isLoading.set(false);
+      },
+      complete: () => {
+        this.isLoading.set(false);
+        this.userInput.set('');
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
